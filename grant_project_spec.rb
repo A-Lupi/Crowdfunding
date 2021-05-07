80b3448e3d056e5915eccc4951ba7a7e2b2ebc9a
A-lupi <LupiAgustinEzequiel@gmail.com>
2021-05-07 10:05:47 -0300

    Add spec for grant project

---
+++ grant_project_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'grant_project'
+
+describe GrantProject do
+  before do
+    @initial_founds = 500
+    @project = GrantProject.new("FGH", 1000, 500)
+  end
+
+  it "does not ever have funds removed" do
+    @project.remove_fund
+    @project.total_funds.should == @initial_founds
+  end
+end
