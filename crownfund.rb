f1dad9958aac9e995fc9dc0de212b04dcc563f6e
A-lupi <LupiAgustinEzequiel@gmail.com>
2021-05-18 14:20:26 -0300

    comment unused code

--- crownfund.rb
+++ crownfund.rb
@@ -1,9 +1,14 @@
 require_relative 'project'
 require_relative 'fundrequest'
 
-project1 = Project.new("ABC", 1000, 100)
-project2 = Project.new("LMN", 2000, 200)
-project3 = Project.new("XYZ", 3000, 300)
+#require_relative 'grant_project'
+#require_relative 'matching_project'
+
+#project1 = Project.new("ABC", 1000, 100)
+#project2 = Project.new("LMN", 2000, 200)
+#project3 = Project.new("XYZ", 3000, 300)
+#grant1 = GrantProject.new("Grant 123", 500, 100)
+#match1 = MatchingProject.new("Match Me", 200, 25)
 
 vc_friendly = Fund_request.new("VC-Friendly Start-up Projects" )
 vc_friendly.load_players(ARGV.shift || "projects.csv")
