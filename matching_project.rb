8202e015831c21bebcd37a2114c766bfb32883cb
A-lupi <LupiAgustinEzequiel@gmail.com>
2021-05-18 00:05:54 -0300

    Fix typo

--- matching_project.rb
+++ matching_project.rb
@@ -2,16 +2,16 @@ require_relative 'project'
 
 class MatchingProject < Project
   
-  def initialize(name, target_funding_amount, funding=0)
-    super(name, target_funding_amount, funding)
-    @halfway_funded = target_funding_amount / 2
+  def initialize(name, goal, funding=0)
+    super(name, goal, funding)
+    @halfway_funded = goal / 2
   end
   
   def halfway_funded?
     @halfway_funded <= funding
   end
   
-  def add_funds
+  def add_fund
     if halfway_funded?
       @funding += (25*2)
       puts "#{@name} has received at least half its funding!" if halfway_funded?
