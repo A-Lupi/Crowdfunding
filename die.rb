434b2c356680a5c4036d193764499933b2472bca
A-lupi <LupiAgustinEzequiel@gmail.com>
2021-05-18 14:24:15 -0300

    Add reader attribute

--- die.rb
+++ die.rb
@@ -1,5 +1,6 @@
 class Die
-    
+    attr_reader :number
+
     def initialize 
         roll
     end
@@ -8,4 +9,11 @@ class Die
         @number = rand(1..6)
     end
 
+end
+
+if __FILE__ == $0
+  die = Die.new
+  puts die.roll
+  puts die.roll
+  puts die.roll
 end
